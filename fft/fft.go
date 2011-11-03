96d00d5abcf0b82ec63966ab909fc9128a3fddee
Matt Jibson <matt.jibson@gmail.com>
2011-11-03 01:18:18 -0600

    Gofmt

--- fft/fft.go
+++ fft/fft.go
@@ -24,12 +24,12 @@ func Fft(x []float64) []complex128 {
 	lx := len(x)
 
 	// expand a length of a power of 2
-	if lx & (lx - 1) != 0 { // not a power of 2
+	if lx&(lx-1) != 0 { // not a power of 2
 		nl := int(math.Pow(2, math.Ceil(math.Log2(float64(lx)))))
 		nx := make([]float64, nl)
 		copy(nx, x)
-		for i := 0; i < nl - lx; i++ {
-			nx[i + lx] = 0
+		for i := 0; i < nl-lx; i++ {
+			nx[i+lx] = 0
 		}
 		x = nx[:]
 		lx = nl
@@ -44,26 +44,24 @@ func Fft(x []float64) []complex128 {
 
 	// split into even and odd parts
 	for n := 0; n < lx_2; n++ {
-		r[n] = complex(x[n * 2], 0)
-		r[n + lx_2] = complex(x[n * 2 + 1], 0)
+		r[n] = complex(x[n*2], 0)
+		r[n+lx_2] = complex(x[n*2+1], 0)
 	}
 
 	for i := 0; i < int(math.Log2(float64(lx))); i++ {
-		stage := 1 << uint(i + 1)
+		stage := 1 << uint(i+1)
 
-		// 2-point transforms
-		if i == 0 {
+		if i == 0 { // 2-point transforms
 			for n := 0; n < lx_2; n++ {
-				t[n * 2] = r[n * 2] + r[n * 2 + 1]
-				t[n * 2 + 1] = r[n * 2] - r[n * 2 + 1]
+				t[n*2] = r[n*2] + r[n*2+1]
+				t[n*2+1] = r[n*2] - r[n*2+1]
 			}
-		// >2-point transforms
-		} else {
+		} else { // >2-point transforms
 			blocks := lx / stage
 			s_2 := stage / 2
 
 			for j := 1; j < s_2; j++ {
-				if j * 4 == stage {
+				if j*4 == stage {
 					w[j] = complex(0, -1)
 				} else {
 					sin, cos := math.Sincos(-2 * math.Pi / float64(stage) * float64(j))
@@ -74,9 +72,9 @@ func Fft(x []float64) []complex128 {
 			for n := 0; n < blocks; n++ {
 				nb := n * stage
 				for j := 0; j < s_2; j++ {
-					w_n := r[j + nb + s_2] * w[j]
-					t[j + nb      ] = r[j + nb] + w_n
-					t[j + nb + s_2] = r[j + nb] - w_n
+					w_n := r[j+nb+s_2] * w[j]
+					t[j+nb] = r[j+nb] + w_n
+					t[j+nb+s_2] = r[j+nb] - w_n
 				}
 			}
 		}
