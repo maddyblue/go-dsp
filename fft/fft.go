33c29e0a81ff80c28f82ea462444c2aa00f15e76
Matt Jibson <matt.jibson@gmail.com>
2011-11-05 23:00:45 -0600

    Use a global table for the complex factors
    Enable the 2-point optimization which was incorrectly disabled.

--- fft/fft.go
+++ fft/fft.go
@@ -20,8 +20,34 @@ import (
 	"math"
 )
 
+var factors = map[int][]complex128{}
+var inv_factors = map[int][]complex128{}
+
+// Ensures the complex multiplication factors exist for an input array of length input_len.
+func ensureFactors(input_len int) {
+	var cos, sin float64
+	for i := 4; i <= input_len; i <<= 1 {
+		if _, present := factors[i]; !present {
+			factors[i]= make([]complex128, i)
+			inv_factors[i] = make([]complex128, i)
+			for n := 0; n < i; n++ {
+				if n == 0 {
+					sin, cos = 0, 1
+				} else if n*4 == i {
+					sin, cos = -1, 0
+				} else {
+					sin, cos = math.Sincos(-2 * math.Pi / float64(i) * float64(n))
+				}
+				factors[i][n] = complex(cos, sin)
+				inv_factors[i][n] = complex(cos, -sin)
+			}
+		}
+	}
+}
+
 func Fft(x []float64) []complex128 {
 	lx := len(x)
+	ensureFactors(lx)
 
 	// todo: non-hack handling length <= 1 cases
 	if lx <= 1 {
@@ -48,8 +74,6 @@ func Fft(x []float64) []complex128 {
 
 	r := make([]complex128, lx)   // result
 	t := make([]complex128, lx)   // temp
-	w := make([]complex128, lx_2) // complex multipliers
-	w[0] = complex(1, 0)
 
 	for n, v := range x {
 		r[n] = complex(v, 0)
@@ -72,7 +96,7 @@ func Fft(x []float64) []complex128 {
 	}
 
 	for stage := 2; stage <= lx; stage <<= 1 {
-		if stage == 1 { // 2-point transforms
+		if stage == 2 { // 2-point transforms
 			for n := 0; n < lx_2; n++ {
 				t[n*2] = r[n*2] + r[n*2+1]
 				t[n*2+1] = r[n*2] - r[n*2+1]
@@ -81,19 +105,10 @@ func Fft(x []float64) []complex128 {
 			blocks := lx / stage
 			s_2 := stage / 2
 
-			for j := 1; j < s_2; j++ {
-				if j*4 == stage {
-					w[j] = complex(0, -1)
-				} else {
-					sin, cos := math.Sincos(-2 * math.Pi / float64(stage) * float64(j))
-					w[j] = complex(cos, sin)
-				}
-			}
-
 			for n := 0; n < blocks; n++ {
 				nb := n * stage
 				for j := 0; j < s_2; j++ {
-					w_n := r[j+nb+s_2] * w[j]
+					w_n := r[j+nb+s_2] * factors[stage][j]
 					t[j+nb] = r[j+nb] + w_n
 					t[j+nb+s_2] = r[j+nb] - w_n
 				}
